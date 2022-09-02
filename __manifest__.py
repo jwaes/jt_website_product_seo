2ea9a228710e909d147131f632694e4c3fe25a12
jwaes <jaco.waes@gmail.com>
2022-09-02 13:32:50 +0000

    license

--- __manifest__.py
+++ __manifest__.py
@@ -2,22 +2,21 @@
 {
     'name': "jt_website_product_seo",
 
-    'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
 
-    'description': """
-        Long description of module's purpose
-    """,
+    'summary': "Website sale changes for Google search console products",
 
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
+    'description': "",
+
+    'author': "jaco tech",
+    'website': "https://jaco.tech",
+    "license": "AGPL-3",
+
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Uncategorized',
-    'version': '0.1',
+    'version': '0.2',
 
     # any module necessary for this one to work correctly
     'depends': ['base',"website_sale"],
