5b1001efb94bb5f8a43f5a9d1382430cd3665a6b
jwaes <jaco.waes@gmail.com>
2022-09-02 13:15:20 +0000

    initial

---
+++ __manifest__.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "jt_website_product_seo",
+
+    'summary': """
+        Short (1 phrase/line) summary of the module's purpose, used as
+        subtitle on modules listing or apps.openerp.com""",
+
+    'description': """
+        Long description of module's purpose
+    """,
+
+    'author': "My Company",
+    'website': "http://www.yourcompany.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base',"website_sale"],
+
+    # always loaded
+    'data': [
+        'views/website_sale_templates.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+
+    ],
+}
