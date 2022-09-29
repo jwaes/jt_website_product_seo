70f6c4e510341941a519c50d7c892a7615512784
jwaes <jaco.waes@gmail.com>
2022-09-29 14:32:41 +0000

    pixel viewContent

--- __manifest__.py
+++ __manifest__.py
@@ -16,10 +16,10 @@
     # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Uncategorized',
-    'version': '0.8',
+    'version': '0.10',
 
     # any module necessary for this one to work correctly
-    'depends': ['base',"website_sale"],
+    'depends': ['base', "website_sale"],
 
     # always loaded
     'data': [
@@ -31,4 +31,12 @@
     'demo': [
 
     ],
+    'assets': {
+        'web.assets_frontend': [
+            'sale/static/src/js/variant_mixin.js',
+            'website_sale/static/src/js/variant_mixin.js',
+            'jt_website_product_seo/static/src/js/pixel.js',
+            'jt_website_product_seo/static/src/js/variant_mixin.js',
+        ],
+    },
 }
---
+++ static/src/js/variant_mixin.js
@@ -0,0 +1,43 @@
+odoo.define('jt_website_product_seo.VariantMixin', function (require) {
+    'use strict';
+
+    var VariantMixin = require('sale.VariantMixin');
+    var publicWidget = require('web.public.widget');
+
+    require('website_sale.website_sale');
+
+    /**
+     * Addition to the variant_mixin._onChangeCombination
+     *
+     * This will prevent the user from selecting a quantity that is not available in the
+     * stock for that product.
+     *
+     * It will also display various info/warning messages regarding the select product's stock.
+     *
+     * This behavior is only applied for the web shop (and not on the SO form)
+     * and only for the main product.
+     *
+     * @param {MouseEvent} ev
+     * @param {$.Element} $parent
+     * @param {Array} combination
+     */
+    VariantMixin._onChangeCombinationProductPixel = function (ev, $parent, combination) {
+        window.fbq('track', 'ViewContent');
+    };
+
+
+    publicWidget.registry.WebsiteSale.include({
+        /**
+         * Adds the product properties updating to the regular _onChangeCombination method
+         * @override
+         */
+        _onChangeCombination: function () {
+            this._super.apply(this, arguments);
+            VariantMixin._onChangeCombinationProductPixel.apply(this, arguments);
+        },
+
+    });
+
+    return VariantMixin;
+
+});
