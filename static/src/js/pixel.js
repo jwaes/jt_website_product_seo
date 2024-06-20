97a0ae25182357068d45bd940a8e9cd35248be09
jwaes <jaco.waes@gmail.com>
2024-06-20 11:11:16 +0000

    d

--- static/src/js/pixel.js
+++ static/src/js/pixel.js
@@ -1,84 +1,81 @@
-odoo.define('jt_website_product_seo.tracking', function (require) {
+/** @odoo-module **/
 
-    var publicWidget = require('web.public.widget');
-
-    publicWidget.registry.websiteSaleTrackingMetaPixel = publicWidget.Widget.extend({
-        selector: '.oe_website_sale',
-        events: {
-            'click form[action="/shop/cart/update"] a.a-submit': '_onAddProductIntoCart',
-            'click a[href="/shop/checkout"]': '_onCheckoutStart',
-            'view_item_event': '_onViewItemX',
-            'add_to_cart_event': '_onAddToCart',
-        },
+import publicWidget from '@web/legacy/js/public/public_widget';
 
 
-        /**
-         * @override
-         */
-        start: function () {
-            var self = this;
-
-            // ...
-            const $confirmation = this.$('div.oe_website_sale_tx_status');
-            if ($confirmation.length) {
-                const json = $confirmation.data('order-tracking-info');
-                self._trackMetaPixel('track', 'purchase', json);
-            }
-
-            return this._super.apply(this, arguments);
-        },
-
-        //--------------------------------------------------------------------------
-        // Private
-        //--------------------------------------------------------------------------
-
-        /**
-         * @private
-         */
-        _trackMetaPixel: function () {
-            if(window.fbq){
-                const websiteFBQ = window.fbq || function () { };
-                websiteFBQ.apply(this, arguments);
-            };
-        },
+publicWidget.registry.websiteSaleTrackingMetaPixel = publicWidget.Widget.extend({
+    selector: '.oe_website_sale',
+    events: {
+        'click form[action="/shop/cart/update"] a.a-submit': '_onAddProductIntoCart',
+        'click a[href="/shop/checkout"]': '_onCheckoutStart',
+        'view_item_event': '_onViewItemX',
+        'add_to_cart_event': '_onAddToCart',
+    },
 
 
+    /**
+     * @override
+     */
+    start: function () {
+        var self = this;
 
-        //--------------------------------------------------------------------------
-        // Handlers
-        //--------------------------------------------------------------------------
+        // ...
+        const $confirmation = this.$('div.oe_website_sale_tx_status');
+        if ($confirmation.length) {
+            const json = $confirmation.data('order-tracking-info');
+            self._trackMetaPixel('track', 'purchase', json);
+        }
 
-        /**
-         * @private
-         */
-        _onViewItemX(event, ...productsTrackingInfo) {
-            this._trackMetaPixel('track', 'ViewContent');
-        },
+        return this._super.apply(this, arguments);
+    },
 
-        /**
-         * @private
-         */
-        _onAddToCart(event, ...productsTrackingInfo) {
-            this._trackMetaPixel('track', 'AddToCart');
-        },
+    //--------------------------------------------------------------------------
+    // Private
+    //--------------------------------------------------------------------------
 
-        /**
-         * @private
-         */
-        _onAddProductIntoCart: function () {
-            this._trackMetaPixel('track', 'AddToCart');
-        },
-        /**
-         * @private
-         */
-        _onCheckoutStart: function () {
-            this._trackMetaPixel('track', 'InitiateCheckout');
-        },
+    /**
+     * @private
+     */
+    _trackMetaPixel: function () {
+        if (window.fbq) {
+            const websiteFBQ = window.fbq || function () { };
+            websiteFBQ.apply(this, arguments);
+        };
+    },
 
 
-    });
 
+    //--------------------------------------------------------------------------
+    // Handlers
+    //--------------------------------------------------------------------------
 
-    return publicWidget.registry.websiteSaleTrackingMetaPixel;
+    /**
+     * @private
+     */
+    _onViewItemX(event, ...productsTrackingInfo) {
+        this._trackMetaPixel('track', 'ViewContent');
+    },
+
+    /**
+     * @private
+     */
+    _onAddToCart(event, ...productsTrackingInfo) {
+        this._trackMetaPixel('track', 'AddToCart');
+    },
+
+    /**
+     * @private
+     */
+    _onAddProductIntoCart: function () {
+        this._trackMetaPixel('track', 'AddToCart');
+    },
+    /**
+     * @private
+     */
+    _onCheckoutStart: function () {
+        this._trackMetaPixel('track', 'InitiateCheckout');
+    },
+
 
 });
+
--- static/src/js/variant_mixin.js
+++ static/src/js/variant_mixin.js
@@ -1,45 +1,16 @@
-odoo.define('jt_website_product_seo.VariantMixin', function (require) {
-    'use strict';
+/** @odoo-module **/
 
-    var VariantMixin = require('sale.VariantMixin');
-    var publicWidget = require('web.public.widget');
+import VariantMixin from "@website_sale/js/variant_mixin";
 
-    require('website_sale.website_sale');
+const originalOnChangeCombination = VariantMixin._onChangeCombination;
 
-    /**
-     * Addition to the variant_mixin._onChangeCombination
-     *
-     * This will prevent the user from selecting a quantity that is not available in the
-     * stock for that product.
-     *
-     * It will also display various info/warning messages regarding the select product's stock.
-     *
-     * This behavior is only applied for the web shop (and not on the SO form)
-     * and only for the main product.
-     *
-     * @param {MouseEvent} ev
-     * @param {$.Element} $parent
-     * @param {Array} combination
-     */
-    VariantMixin._onChangeCombinationProductPixel = function (ev, $parent, combination) {
-        if(window.fbq){
-            window.fbq('track', 'ViewContent');
-        };
+VariantMixin._onChangeCombinationProductPixel = function (ev, $parent, combination) {
+    if (window.fbq) {
+        window.fbq('track', 'ViewContent');
     };
 
+    originalOnChangeCombination.apply(this, [ev, $parent, combination]);
+};
 
-    publicWidget.registry.WebsiteSale.include({
-        /**
-         * Adds the product properties updating to the regular _onChangeCombination method
-         * @override
-         */
-        _onChangeCombination: function () {
-            this._super.apply(this, arguments);
-            VariantMixin._onChangeCombinationProductPixel.apply(this, arguments);
-        },
+export default VariantMixin;
 
-    });
-
-    return VariantMixin;
-
-});
